e1cf06e74e71a4081a07103a31d7c76d697f1a80
Md. Rafiqul Islam Mamun <mamun8475@gmail.com>
2021-11-13 00:23:10 +0600

    spell check

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
+const express = require('express');
 const { MongoClient } = require('mongodb');
-const cors = require('cors')
+const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
-require('dotenv').config()
+require('dotenv').config();
 
-const app = express()
+const app = express();
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT || 5000
 
 // connection string
